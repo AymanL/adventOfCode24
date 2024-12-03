4c4f8a4f595a5546660a75d0397bea1589eccf03
AymanL <40838419+AymanL@users.noreply.github.com>
2024-12-03 12:32:47 +0100

    fix

--- day3.py
+++ day3.py
@@ -4,7 +4,6 @@ with open("datasets/day3.txt", "r") as file:
     data = file.read().replace('\n', '')
 
 def mul_match(line):
-    print("line2", line)
     mul_matches = re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', line)
     total = 0
     for mul in mul_matches:
@@ -17,22 +16,17 @@ def mul_advanced_match(line):
     if(not use):
         return 0
     result = mul_match(line)
-    print("result", result)
     return result
 
-mul_matches = re.findall(r'.+?(?=(do\(\)|don\'t\(\)))', data)
 total = 0
-
+left_data = data
 groups = [x.group() for x in re.finditer(r'.+?(?=(do\(\)|don\'t\(\)))', data)]
-i = 1
+
 for group in groups:
-    # print(group)
-    total += mul_advanced_match(group)
-    i += 1
-    if(i==5):
-        break
-    
+    left_data = left_data[len(group):]
+    result = mul_advanced_match(group)
+    print(group, result)
+    total += result
+total += mul_match(left_data)
 print(total)
-# print(168*87 + 911*800+ 734*19+ 829*495)
-# print(168*87)
-# mul(168,87)}*:mul(911,800)(%,)where()#&&$mul(734,19)?when()why(){$:?mul(829,495)
+print(left_data)
